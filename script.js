1f4e094fe6c5b885f766482e925e4ecdac1d79f0
davina-nb <dnb24@njit.edu>
2024-11-11 09:51:11 -0500

    java lecture

---
+++ script.js
@@ -0,0 +1,54 @@
+var age = 20;
+var age;
+age = 100;
+console.log(age);
+
+let username = "Harry";
+username = "Potter";
+console.log(username);
+
+let FirstName = "Davina";
+let LastName = "Brown";
+console.log(FirstName + " " + LastName);
+
+const valuePi = 3.14;
+console.log(valuePi);
+
+let pets = true;
+console.log(pets);
+
+let house;
+console.log(house);
+
+let room = null;
+console.log(room);
+
+let AGE = 8656489482047857483690948597479610209384565020985759302002847n;
+console.log(AGE);
+
+let a = 10;
+let b = 15; 
+console.log(a + b);
+console.log(a/b);
+console.log(a % b);
+//b += 5; ==> b= b+5; 
+//b *= 5; ==> b= b*5; 
+
+console.log(a == b);
+console.log(a != b);
+console.log(a === b);
+console.log(a !== b);
+
+let A = true;
+let B = false;
+console.log(A && B)
+console.log(A || B);
+console.log(!A);
+
+let user = "Harry";
+let home;
+if(user=="Harry"){
+    home = "gryffindor";
+    console.log(home);
+
+}
